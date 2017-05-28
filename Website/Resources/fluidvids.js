707fb309271c80a3e446587a15c26ffdc9cc74be
Dartegnian <dartegnian@users.noreply.github.com>
2017-05-28 21:56:37 +0800

    JS change

--- Website/Resources/fluidvids.js
+++ Website/Resources/fluidvids.js
@@ -1,11 +1,9 @@
-var $allVideos = $("iframe[src^='//www.youtube.com']"),
-    $fluidEl = $("body");
+var $allVideos = $("iframe[src^='//www.youtube.com']"), $fluidEl = $("body");
 $allVideos.each(function() {
     $(this)
         .data('aspectRatio', this.height / this.width)
         .removeAttr('height')
         .removeAttr('width');
-
 });
 $(window).resize(function() {
     var newWidth = $fluidEl.width();
